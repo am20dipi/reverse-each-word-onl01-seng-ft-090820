ee3c0100a0bbf3a86a1515a429494b9b476e14ca
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-11 20:43:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-sentence1 = "Hello there, and how are you?"
+
 def reverse_each_word(sentence)
   new_array= []
-  sentence_array = sentence1.split(", ")
+  sentence_array = sentence.split(", ")
   sentence.each do |words|
     new_array << words.reverse
   end
