41c715f41abc22841d904712498a301726347801
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-11 20:46:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   binding.pry
   new_array= []
   sentence_array = sentence.split(" ")
-  sentence.each do |words|
+  sentence_array.each do |words|
     new_array << words.reverse
   end
   return new_array
