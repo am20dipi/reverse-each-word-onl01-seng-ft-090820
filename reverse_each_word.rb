b25af8cb57a25018629421a9e2a36378e2823042
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-11 20:45:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   binding.pry
   new_array= []
-  sentence_array = sentence.split(", ")
+  sentence_array = sentence.split
   sentence.each do |words|
     new_array << words.reverse
   end
